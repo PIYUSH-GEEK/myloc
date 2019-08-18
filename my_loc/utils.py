7d650b2023ad8b43bf7df8937c17476ece85ae67
PIYUSH-GEEK <53996723+PIYUSH-GEEK@users.noreply.github.com>
2019-08-18 17:11:02 +0530

    Update utils.py

--- my_loc/utils.py
+++ my_loc/utils.py
@@ -1,23 +1,23 @@
 import requests
 def locate(i):
-	my_loc = requests.get('https://ipinfo.io').json()
+	loc = requests.get('https://ipinfo.io').json()
 	
 	if str(i).isdigit():
 		return 'Invalid input. Try a string.'
 	else:
 		if i == 'ip':
-			return my_loc['ip']
+			return loc['ip']
 		elif i == 'city' or i == 'capital':
-			return my_loc['city']
+			return loc['city']
 		elif i == 'region' or i == 'state':
-			return my_loc['region']
+			return loc['region']
 		elif i == 'country':
-			return my_loc['country']
+			return loc['country']
 		elif i == 'loc':
-			return my_loc['loc']
+			return loc['loc']
 		elif i == 'org':
-			return my_loc['org']
+			return loc['org']
 		elif i == 'postal' or i == 'pin':
-			return my_loc['postal']
+			return loc['postal']
 		elif i == 'readme':
-			return my_loc['readme']
+			return loc['readme']
