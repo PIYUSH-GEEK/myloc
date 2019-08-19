1adec00d84d9d5abfaf82338a6fba8965d3069f0
PIYUSH-GEEK <53996723+PIYUSH-GEEK@users.noreply.github.com>
2019-08-19 14:56:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="my-loc",
-    version="1.0",
+    name="myloc",
+    version="1.0.0",
     author="Piyush Kumar",
     author_email="pk33463@gmail.com",
     description="A python package to get the location.",
     long_description= long_description,long_description_content_type="text/markdown",
     url="https://github.com/PIYUSH-GEEK/my-loc",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages = ["my_loc"],
     include_package_data = True,
     install_requires = ["requests"],
 
